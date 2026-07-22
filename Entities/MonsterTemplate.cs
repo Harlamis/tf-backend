872848323c920563844e3ecc5de9828818eb38a8
Yeghor Danyliuk <egordan74@gmail.com>
2026-07-22 23:00:59 +0300

    add MonsterTemplate entity

---
+++ Entities/MonsterTemplate.cs
@@ -0,0 +1,12 @@
+public class MonsterTemplate
+{
+    public String Id {get; set;}
+
+    public String BaseName {get;set;}
+
+    public int maxHp {get;set;}
+
+    public int Ac {get; set;}
+
+    public String? DetailsJson {get; set;}
+}
