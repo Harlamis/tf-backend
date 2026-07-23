ec35a917c6086d05df3a62f9e79c004700f310bc
Yeghor Danyliuk <egordan74@gmail.com>
2026-07-23 13:01:27 +0300

    add initial migration and model snapshot for ApplicationDbContext

---
+++ Migrations/20260723095351_InitialCreate.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Trackfinder.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Encounters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    CurrentRound = table.Column<int>(type: "integer", nullable: false),
+                    ActiveEncounterId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Encounters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MonsterTemplate",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    BaseName = table.Column<string>(type: "text", nullable: false),
+                    maxHp = table.Column<int>(type: "integer", nullable: false),
+                    Ac = table.Column<int>(type: "integer", nullable: false),
+                    DetailsJson = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MonsterTemplate", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CombatMonsters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TemplateId = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    CurrentHp = table.Column<int>(type: "integer", nullable: false),
+                    Init = table.Column<int>(type: "integer", nullable: false),
+                    EncounterId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CombatMonsters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CombatMonsters_Encounters_EncounterId",
+                        column: x => x.EncounterId,
+                        principalTable: "Encounters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CombatMonsters_MonsterTemplate_TemplateId",
+                        column: x => x.TemplateId,
+                        principalTable: "MonsterTemplate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CombatMonsters_EncounterId",
+                table: "CombatMonsters",
+                column: "EncounterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CombatMonsters_TemplateId",
+                table: "CombatMonsters",
+                column: "TemplateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CombatMonsters");
+
+            migrationBuilder.DropTable(
+                name: "Encounters");
+
+            migrationBuilder.DropTable(
+                name: "MonsterTemplate");
+        }
+    }
+}
