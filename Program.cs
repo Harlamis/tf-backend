e81ef0922fb4917b8b83e70af7479536dbd8a8ef
Yeghor Danyliuk <egordan74@gmail.com>
2026-07-23 12:53:39 +0300

    configure database connection and add Entity Framework Core design package

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
