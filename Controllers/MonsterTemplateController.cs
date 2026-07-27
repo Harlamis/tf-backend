17a0106768728fefe37632e9e086fbb3c5d57f31
Yeghor Danyliuk <egordan74@gmail.com>
2026-07-27 18:54:44 +0300

    add MonsterTemplateController for managing template access

---
+++ Controllers/MonsterTemplateController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trackfinder.Api.Controllers;
+
+/// <summary>
+/// Provides access to Templates repository a.k.a Bestiary
+/// </summary>
+[ApiController]
+[Route("api/v1/templates")]
+[Produces("application/json")]
+public class MonsterTemplateController : ControllerBase
+{
+    private readonly IMonsterTemplateService _service;
+
+    public MonsterTemplateController(IMonsterTemplateService service)
+    {
+        _service = service;
+    }
+}
