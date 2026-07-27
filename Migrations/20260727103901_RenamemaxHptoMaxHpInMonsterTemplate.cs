c8670335e1ae706018e8ab2a9df7d7317c4dc70b
Yeghor Danyliuk <egordan74@gmail.com>
2026-07-27 13:39:37 +0300

    rename maxHp to MaxHp in Templates for consistency

---
+++ Migrations/20260727103901_RenamemaxHptoMaxHpInMonsterTemplate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trackfinder.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenamemaxHptoMaxHpInMonsterTemplate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "maxHp",
+                table: "Templates",
+                newName: "MaxHp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MaxHp",
+                table: "Templates",
+                newName: "maxHp");
+        }
+    }
+}
