3e5d403c74f533c1e391e3af8b4a20a18bfe0bfe
Yeghor Danyliuk <egordan74@gmail.com>
2026-07-30 15:11:23 +0300

    feat: add seed data for MonsterTemplate entities in the database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -22,6 +22,105 @@ public class ApplicationDbContext : DbContext
                 .HasMaxLength(100);
         });
 
+        modelBuilder.Entity<MonsterTemplate>().HasData(
+        new MonsterTemplate
+        {
+            Id = "mudcrab-001",
+            BaseName = "Mudcrab",
+            MaxHp = 7,
+            Ac = 12,
+            DetailsJson = """
+            {
+              "level": 1,
+              "traits": ["Beast", "Aquatic"],
+              "speed": {
+                "walking": 20,
+                "flying": 0,
+                "swimming": 20,
+                "burrowing": 10,
+                "climbing": 0
+              },
+              "savingThrows": {
+                "fortitude": 6,
+                "reflex": 4,
+                "will": 2
+              },
+              "attacks": [
+                { "name": "Pincer", "type": "melee", "bonus": 6, "traits": ["agile", "finesse"] }
+              ],
+              "loot": [
+                { "item": "Mudcrab Chitin", "quantity": 1 }
+              ]
+            }
+            """
+        },
+        new MonsterTemplate
+        {
+            Id = "draugr-001",
+            BaseName = "Draugr Deathlord",
+            MaxHp = 85,
+            Ac = 15,
+            DetailsJson = """
+            {
+              "level": 5,
+              "traits": ["Undead", "Mindless"],
+              "speed": {
+                "walking": 25,
+                "flying": 0,
+                "swimming": 0,
+                "burrowing": 0,
+                "climbing": 0
+              },
+              "savingThrows": {
+                "fortitude": 12,
+                "reflex": 8,
+                "will": 10
+              },
+              "attacks": [
+                { "name": "Ebony Greatsword", "type": "melee", "bonus": 14, "traits": ["reach", "sweep"] }
+              ],
+              "loot": [
+                { "item": "Bone Meal", "quantity": 2 },
+                { "item": "Ebony Greatsword", "quantity": 1 }
+              ]
+            }
+            """
+        },
+        new MonsterTemplate
+        {
+            Id = "dragon-001",
+            BaseName = "Ancient Dragon",
+            MaxHp = 250,
+            Ac = 19,
+            DetailsJson = """
+            {
+              "level": 12,
+              "traits": ["Dragon", "Fire"],
+              "speed": {
+                "walking": 30,
+                "flying": 100,
+                "swimming": 0,
+                "burrowing": 0,
+                "climbing": 0
+              },
+              "savingThrows": {
+                "fortitude": 24,
+                "reflex": 20,
+                "will": 22
+              },
+              "attacks": [
+                { "name": "Bite", "type": "melee", "bonus": 26, "traits": ["reach"] },
+                { "name": "Fire Breath", "type": "ranged", "bonus": 20, "traits": ["magical", "fire"] }
+              ],
+              "loot": [
+                { "item": "Dragon Scales", "quantity": 5 },
+                { "item": "Dragon Bone", "quantity": 3 }
+              ]
+            }
+            """
+        }
+    );
+
         modelBuilder.Entity<CombatMonster>(entity =>
         {
             entity.HasOne(m => m.Encounter).WithMany(e => e.Monsters).HasForeignKey(m => m.EncounterId).OnDelete(DeleteBehavior.Cascade);
@@ -30,7 +129,7 @@ public class ApplicationDbContext : DbContext
 
             entity.Property(m => m.TemplateId)
                 .IsRequired()
-                .HasMaxLength(50);            
+                .HasMaxLength(50);
         });
     }
 }
---
+++ Migrations/20260730121036_SeedBestiary.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Trackfinder.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBestiary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Templates",
+                columns: new[] { "Id", "Ac", "BaseName", "DetailsJson", "MaxHp" },
+                values: new object[,]
+                {
+                    { "dragon-001", 19, "Ancient Dragon", "{\r\n  \"level\": 12,\r\n  \"traits\": [\"Dragon\", \"Fire\"],\r\n  \"speed\": {\r\n    \"walking\": 30,\r\n    \"flying\": 100,\r\n    \"swimming\": 0,\r\n    \"burrowing\": 0,\r\n    \"climbing\": 0\r\n  },\r\n  \"savingThrows\": {\r\n    \"fortitude\": 24,\r\n    \"reflex\": 20,\r\n    \"will\": 22\r\n  },\r\n  \"attacks\": [\r\n    { \"name\": \"Bite\", \"type\": \"melee\", \"bonus\": 26, \"traits\": [\"reach\"] },\r\n    { \"name\": \"Fire Breath\", \"type\": \"ranged\", \"bonus\": 20, \"traits\": [\"magical\", \"fire\"] }\r\n  ],\r\n  \"loot\": [\r\n    { \"item\": \"Dragon Scales\", \"quantity\": 5 },\r\n    { \"item\": \"Dragon Bone\", \"quantity\": 3 }\r\n  ]\r\n}", 250 },
+                    { "draugr-001", 15, "Draugr Deathlord", "{\r\n  \"level\": 5,\r\n  \"traits\": [\"Undead\", \"Mindless\"],\r\n  \"speed\": {\r\n    \"walking\": 25,\r\n    \"flying\": 0,\r\n    \"swimming\": 0,\r\n    \"burrowing\": 0,\r\n    \"climbing\": 0\r\n  },\r\n  \"savingThrows\": {\r\n    \"fortitude\": 12,\r\n    \"reflex\": 8,\r\n    \"will\": 10\r\n  },\r\n  \"attacks\": [\r\n    { \"name\": \"Ebony Greatsword\", \"type\": \"melee\", \"bonus\": 14, \"traits\": [\"reach\", \"sweep\"] }\r\n  ],\r\n  \"loot\": [\r\n    { \"item\": \"Bone Meal\", \"quantity\": 2 },\r\n    { \"item\": \"Ebony Greatsword\", \"quantity\": 1 }\r\n  ]\r\n}", 85 },
+                    { "mudcrab-001", 12, "Mudcrab", "{\r\n  \"level\": 1,\r\n  \"traits\": [\"Beast\", \"Aquatic\"],\r\n  \"speed\": {\r\n    \"walking\": 20,\r\n    \"flying\": 0,\r\n    \"swimming\": 20,\r\n    \"burrowing\": 10,\r\n    \"climbing\": 0\r\n  },\r\n  \"savingThrows\": {\r\n    \"fortitude\": 6,\r\n    \"reflex\": 4,\r\n    \"will\": 2\r\n  },\r\n  \"attacks\": [\r\n    { \"name\": \"Pincer\", \"type\": \"melee\", \"bonus\": 6, \"traits\": [\"agile\", \"finesse\"] }\r\n  ],\r\n  \"loot\": [\r\n    { \"item\": \"Mudcrab Chitin\", \"quantity\": 1 }\r\n  ]\r\n}", 7 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Templates",
+                keyColumn: "Id",
+                keyValue: "dragon-001");
+
+            migrationBuilder.DeleteData(
+                table: "Templates",
+                keyColumn: "Id",
+                keyValue: "draugr-001");
+
+            migrationBuilder.DeleteData(
+                table: "Templates",
+                keyColumn: "Id",
+                keyValue: "mudcrab-001");
+        }
+    }
+}
